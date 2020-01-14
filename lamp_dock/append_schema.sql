9ba81f4276ca902bb1379cdd224a14991b0b245d
Erika Yabui <yerika.1127@gmail.com>
2020-01-14 11:20:48 +0900

    issue/1テーブル2修正、コメント修正

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -1,16 +1,3 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
--- 自動的にcommit文が実行される機能を無効にする
-SET AUTOCOMMIT = 0;
--- 自動的にcommitされるのを防ぎ、ユーザでcommit/rollback文を実行する
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-CREATE TABLE `history` (
-  `order_id` int(11) NOT NULL,  
-  `user_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL,
-  `amount` int(11) NOT NULL,
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `sample`.`orders` ( `order_id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , `updated` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`order_id`)) ENGINE = InnoDB;
+CREATE TABLE `sample`.`orders` ( `detail_id` INT NOT NULL AUTO_INCREMENT , `order_id` INT NOT NULL , `item_id` INT NOT NULL , `amount` INT NOT NULL , `price_bought` INT NOT NULL , `created` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , `updated` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`detail_id`)) ENGINE = InnoDB;
 
