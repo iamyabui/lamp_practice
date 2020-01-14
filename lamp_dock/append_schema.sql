73db116d429bf4fa88f6c07bbb9ad3fe3e51c8ef
Erika Yabui <yerika.1127@gmail.com>
2020-01-14 14:34:59 +0900

    issue/12テーブル修正

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -1,3 +1,19 @@
-CREATE TABLE `sample`.`orders` ( `order_id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , `updated` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`order_id`)) ENGINE = InnoDB;
-CREATE TABLE `sample`.`orders` ( `detail_id` INT NOT NULL AUTO_INCREMENT , `order_id` INT NOT NULL , `item_id` INT NOT NULL , `amount` INT NOT NULL , `price_bought` INT NOT NULL , `created` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , `updated` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`detail_id`)) ENGINE = InnoDB;
+CREATE TABLE `sample`.`orders` ( 
+     `order_id` INT NOT NULL AUTO_INCREMENT ,
+     `user_id` INT NOT NULL , 
+     `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+     `updated` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+     PRIMARY KEY (`order_id`)
+     ) ENGINE = InnoDB;
+
+CREATE TABLE `sample`.`orders` ( 
+    `detail_id` INT NOT NULL AUTO_INCREMENT , 
+    `order_id` INT NOT NULL , 
+    `item_id` INT NOT NULL , 
+    `amount` INT NOT NULL , 
+    `price_bought` INT NOT NULL , 
+    `created` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    `updated` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    PRIMARY KEY (`detail_id`)
+    ) ENGINE = InnoDB;
 
